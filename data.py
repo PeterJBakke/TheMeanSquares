7e75166e84175f012b61336e9258b73c14b36b80
PeterJBakke <43293698+PeterJBakke@users.noreply.github.com>
2018-11-05 14:38:54 +0100

    data loader for ratings data

--- data.py
+++ data.py
@@ -1,3 +1,17 @@
 """
 Data preprocessing file
 """
+
+import pandas as pd
+import os
+
+base_dir = os.getcwd()
+
+def MovieLensRatingsData(path=base_dir + '/Datasets/MovieLens-Small/ratings.csv'):
+    data = pd.read_csv(path)
+    return data
+
+if __name__ == "__main__":
+    myData = MovieLensRatingsData()
+    print(myData)
+
