c96c9239cd6b482885354cbca3d1feee48439609
PeterJBakke <43293698+PeterJBakke@users.noreply.github.com>
2018-11-05 15:11:04 +0100

    created MovieLens class

--- data.py
+++ data.py
@@ -5,18 +5,56 @@ Data preprocessing file
 import pandas as pd
 import os
 
-base_dir = os.getcwd()
 
-def MovieLensRatingsData(path=base_dir + '/Datasets/MovieLens-Small/ratings.csv'):
-    """
-    Data loader for the Movie Lens Ratings data
-    :param path: Directory where data is located
-    :return: pandas dataframe with MovieLens Ratings data
-    """
-    data = pd.read_csv(path)
-    return data
+
+class MovieLens():
+    def __init__(self):
+        self.base_dir = os.getcwd()
+        self.ratingsPath = self.base_dir + '/Datasets/MovieLens-Small/ratings.csv'
+        self.moviesPath = self.base_dir + '/Datasets/MovieLens-Small/movies.csv'
+
+    def MovieLensRatingsData(self):
+        """
+        Method for loading ratings data
+        :return: data frame with MovieLens ratings data
+        """
+        path = self.ratingsPath
+        data = pd.read_csv(path)
+        return data
+
+    def MovieLensMoviesData(self):
+        """
+        Method for loading movies data
+        :return: data frame with MovieLens movies data
+        """
+        path = self.moviesPath
+        data = pd.read_csv(path)
+        return data
+
+    def MovieLensUsers(self):
+        """
+        Method for getting a list of unique userId's
+        :return: DataFrame with the userId's from the MovieLens DataSet
+        """
+        df = self.MovieLensRatingsData()
+        df = df['userId']
+        df.drop_duplicates( inplace=True)
+        return df
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    myData = MovieLensRatingsData()
-    print(myData)
+    myData = MovieLens()
+    movies = myData.MovieLensMoviesData()
+    ratings = myData.MovieLensRatingsData()
+    users = myData.MovieLensUsers()
+    print(movies.shape)
+    print(ratings.shape)
+    print(users.shape)
 
