bc11c23b2600515652a56e34b156b2fb9110e688
cralle <cralle@outlook.com>
2018-11-07 15:15:39 +0100

    Added Datahandler

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # TheMeanSquares
 Repository for the final report in 02456 Deep Learning at DTU for the group TheMeanSquares.
 
-
+## Install
+pip install -r requirements.txt
--- data.py
+++ data.py
@@ -24,8 +24,8 @@ class MovieLens():
     """
     def __init__(self):
         self.base_dir = os.getcwd()
-        self.ratingsPath = self.base_dir + '/Datasets/MovieLens-Small/ratings.csv'
-        self.moviesPath = self.base_dir + '/Datasets/MovieLens-Small/movies.csv'
+        self.ratingsPath = os.path.join(self.base_dir, '/Datasets/MovieLens-Small/ratings.csv')
+        self.moviesPath = os.path.join(self.base_dir, '/Datasets/MovieLens-Small/movies.csv')
         print("MovieLens Data handler started \n")
         print("Path to ratings data: \n" + str(self.ratingsPath))
         print("Path to movies data: \n" + str(self.moviesPath))
@@ -58,13 +58,61 @@ class MovieLens():
         df.drop_duplicates( inplace=True)
         return df
 
+class TalentFox():
+    """
+    Class to handle the TalentFox data
 
+    Predict:
+    match_status
 
+    Columns for candidate:
+    candidate_city, candidate_state, candidate_country, candidate_title, candidate_birth_date,
+    candidate_current_fixed_salary, candidate_current_bonus_salary, candidate_in_job_market_since,
+    candidate_other_languages, candidate_is_looking_for_new_job, candidate_wish_2, candidate_wish_3, candidate_wish_1,
+    candidate_education, candidate_language_negotiative, candidate_language_basic, candidate_language_fluent,
+    candidate_highest_degree, candidate_career_type, candidate_industries, candidate_professions, candidate_resume,
+    candidate_feedback, candidate_professions_global, candidate_industries_global, candidate_relocation_ready,
 
+    Columns for job:
+    job_fixed_salary, job_bonus_salary, job_title, job_vacation_days, job_needed_experience, job_language,
+    job_description, job_daily_tasks_of_job, job_required_experience_of_candidate,
+    job_preferred_experience_of_candidate, job_preferred_education_of_candidate, job_max_candidate_age,
+    job_min_candidate_age, job_company_structure, job_language_skills_negotiative, job_language_skills_basic,
+    job_candidate_radius, job_candidate_relocation, job_city, job_state, job_country, job_time_model, job_max_salary,
+    job_questions_for_candidate, match_employer_feedback
+    """
+    def __init__(self):
+        self.base_dir = os.getcwd()
+        self.file = os.path.join(self.base_dir, 'Datasets/talentfox_match_data/processed_dataset.csv')
+        self.data = self.load(self.file)
 
+    def load(self, file):
+        data = pd.read_csv(file)
+        return data
 
+    def getJobRequiredExperience(self):
+        return self.data['job_required_experience_of_candidate']
 
+    def getJobDescription(self):
+        return self.data['job_description']
 
+    def getJobDailyTasks(self):
+        return self.data['job_daily_tasks_of_job']
+
+    def getJobTitles(self):
+        return self.data['job_title']
+
+    def getCandidateResume(self):
+        return self.data['candidate_resume']
+
+    def getCandidateProfessions(self):
+        return self.data['candidate_professions']
+
+    def getCandidateTitle(self):
+        return self.data['candidate_title']
+
+    def getMatchStatus(self):
+        return self.data['match_status']
 
 if __name__ == "__main__":
     myData = MovieLens()
@@ -74,4 +122,3 @@ if __name__ == "__main__":
     print(movies.shape)
     print(ratings.shape)
     print(users.shape)
-
