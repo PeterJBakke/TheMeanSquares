4ab3bfddf498540b2155c4fd8efbfca26964a19d
Daniel Horvath <dhorvath19@gmail.com>
2018-11-26 13:30:36 +0100

    citeulike model - under construction

--- model.py
+++ model.py
@@ -97,3 +97,70 @@ class CiteulikeNet(nn.Module):
         x = self.lin1(x)
         x = self.lin2(x)
         return torch.sigmoid(x) * (max_rating - min_rating + 1) + min_rating - 0.5
+
+
+class CiteULikeModel(nn.Module):
+    """
+    Colaboratie filtering model for article-author paring
+    """
+
+    def __init__(self, article_field, author_field, author_dim=10, l1=50, l2=50, p1=0.3, p2=0.3, p3=0.3):
+        """
+
+        :param article_field: Field for the article texts
+        :type article_field: torchtext.data.Field
+        :param author_field: Field for authors
+        :type author_field: torchtext.data.Field
+        :param author_dim: Dimensionality of the author embedding
+        :param l1: Number of hidden units in the 1st layer
+        :param l2: Number of hidden units in the 2nd layer
+        :param p1: Dropout probability for the 1st layer
+        :param p2: Dropout probability for the 2nd layer
+        :param p3: Dropout probability in the output layer
+        """
+        super(CiteULikeModel, self).__init__()
+
+        article_vectors = article_field.vocab.vectors
+        num_embeddings = article_vectors.size()[0]
+        embedding_dim = article_vectors.size()[1]
+
+        self.article_embeddings = nn.Embedding(num_embeddings, embedding_dim)
+        self.article_embeddings.weight.data.copy_(article_vectors)
+
+        num_author = len(author_field.vocab.freqs)
+        self.author_embedding = nn.Embedding(num_author, author_dim)
+        self.author_embedding.weight.data.uniform_(0, 200)
+
+        self.l_1 = nn.Sequential(
+            nn.Dropout(p1),
+            nn.Linear(in_features=(embedding_dim + author_dim),
+                      out_features=l1,
+                      bias=True),
+            nn.ReLU(),
+        )
+
+        self.l_2 = nn.Sequential(
+            nn.Dropout(p2),
+            nn.Linear(in_features=l1,
+                      out_features=l2,
+                      bias=True),
+            nn.ReLU(),
+        )
+
+        self.l_out = nn.Sequential(
+            nn.Dropout(p3),
+            nn.Linear(in_features=l2,
+                      out_features=1,
+                      bias=True),
+        )
+
+    def forward(self, x):
+        author = self.author_embedding(x.author)
+        text = torch.mean(self.article_embeddings(x.text), dim=0)
+        x = torch.cat((author, text), 1)
+
+        x = self.l_1(x)
+        x = self.l_2(x)
+
+        out = torch.sigmoid(self.l_out(x))
+        return out
--- train.py
+++ train.py
@@ -1,5 +1,9 @@
+from types import SimpleNamespace
+
 import torch
 import random
+
+from torch.autograd import Variable
 
 
 def accuracy(output, target):
@@ -7,7 +11,7 @@ def accuracy(output, target):
     return torch.mean(correct_prediction.float())
 
 
-def train(train_iter, val_iter, net, test_iter, optimizer, criterion, num_epochs=5):
+def movie_lens_train(train_iter, val_iter, net, test_iter, optimizer, criterion, num_epochs=5):
     net.train()
     prev_epoch = 0
 
@@ -41,12 +45,88 @@ def train(train_iter, val_iter, net, test_iter, optimizer, criterion, num_epochs
             break
 
 
-def negative_sampling(batch, num_user, device):
-    random_users = [random.randint(1, num_user) for _ in range(len(batch))]
-    rating = [0 for _ in range(len(batch))]
+def cite_u_like_train(train_iter, val_iter, net, test_iter, optimizer, criterion, num_auth, num_epochs=5):
+    net.train()
+    prev_epoch = 0
+    train_loss, train_accs, train_length = [0, 0, 0]
+    train_res = []
+    val_res = []
+    for batch in train_iter:
+        if (train_iter.epoch != prev_epoch) & (train_iter.epoch % 5 != 0):
+            train_loss /= train_length
+            train_accs /= train_length
+            train_res.append(train_accs)
+            val_res.append(None)
+            train_loss, train_accs, train_length = [0, 0, 0]
 
-    user_tensor = torch.tensor(random_users).to(device)
-    rating_tensor = torch.tensor(rating).to(device)
-    batch.user = user_tensor
-    batch.rating = rating_tensor
-    return batch
+        if (train_iter.epoch != prev_epoch) & (train_iter.epoch % 5 == 0):
+            net.eval()
+            val_loss, val_accs, val_length = [0, 0, 0]
+
+            for val_batch in val_iter:
+                val_output = net(val_batch).reshape(-1)
+                val_target = Variable(torch.tensor([1 for _ in range(len(val_batch))]).float().cuda())
+                val_loss += criterion(val_output, val_target) * val_batch.batch_size
+                val_accs += accuracy_sigmoid(val_output, val_target) * val_batch.batch_size
+                val_length += val_batch.batch_size
+
+            val_loss /= val_length
+            val_accs /= val_length
+            val_res.append(val_accs)
+
+            train_loss /= train_length
+            train_accs /= train_length
+            train_res.append(train_accs)
+            print(
+                "Epoch {}: Train loss: {:.2f}, Train avg error: {:.2f} Validation loss: {:.2f}, Validation avg error: {:.2f}"
+                    .format(train_iter.epoch, train_loss, train_accs, val_loss, val_accs))
+            # plot_res(train_res, val_res, train_iter.epoch)
+
+            net.train()
+
+        net.train()
+
+        pos_and_neg_batch = negative_sampling(batch, num_auth)
+        target_pos = Variable(torch.tensor([1 for _ in range(len(batch))]).cuda())
+        target_neg = Variable(torch.tensor([0 for _ in range(len(batch))]).cuda())
+
+        output = net(pos_and_neg_batch).reshape(-1)
+        target = torch.cat((target_pos, target_neg), 0).float().cuda()
+        batch_loss = criterion(output, target)
+        optimizer.zero_grad()
+        batch_loss.backward()
+        optimizer.step()
+
+        # print(
+        #     "Train loss: {:.3f}, Train avg error: {:.3f}"
+        #         .format(criterion(output, target), accuracy_sigmoid(output, target)))
+
+        train_loss += criterion(output, target) * batch.batch_size * 2
+        train_accs += accuracy_sigmoid(output, target) * batch.batch_size * 2
+        train_length += batch.batch_size * 2
+
+        prev_epoch = train_iter.epoch
+        if train_iter.epoch == num_epochs:
+            break
+
+
+def negative_sampling(batch, num_authors):
+    random_authors = torch.tensor(
+        [random.randint(0, num_authors) for _ in range(len(batch))]
+    ).cuda()
+
+    author = torch.cat((batch.author, random_authors), 0).cuda()
+    text = torch.cat((batch.text, batch.text), 1).cuda()
+
+    batch_with_negative_sampling = {'author': author, 'text': text}
+    return SimpleNamespace(**batch_with_negative_sampling)
+
+
+def accuracy_sigmoid(output, target):
+    return torch.mean(torch.abs(output - target).float())
+
+
+def print_params(net):
+    for name, param in net.named_parameters():
+        if param.requires_grad:
+            print(name, param.data)
